40d3dd5b5eef80ce99a913e397f9dbc0675b5462
Guido Geurs <57041484+DenToetie@users.noreply.github.com>
2024-10-11 11:33:48 +0200

    Initiële bestanden voor MakeCode-project

---
+++ Uitleg_24-10-11_01.ts
@@ -0,0 +1,11 @@
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#efefef icon="\uf27a"
+namespace Uitleg {
+    /**
+     * block with comment
+     */
+    //% block
+    export function Comment(s: string): void { }
+}
